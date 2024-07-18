5b174b4f2f421169bf6a4f4f5220b13df6de2ca0
Dmytro Roshchupkin <up.rivers@gmail.com>
2024-07-18 16:17:02 +0200

    Configure eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,5 @@
+const { version } = require("react")
+
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
@@ -5,15 +7,28 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/strict-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked",
-    "plugin:react-hooks/recommended"
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime"
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json", "./tsconfig.app.json"],
+    tsconfigRootDir: __dirname
+  },
   plugins: ["react-refresh"],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true }
     ]
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
   }
 }
